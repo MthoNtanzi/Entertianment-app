ea945e27abca4903d10ddf02c906078040d96394
MthoNtanzi <mthontanzi99@gmail.com>
2026-02-10 10:37:09 +0200

    Add homepage styles and searchbar

--- scripts/pages/homePage.js
+++ scripts/pages/homePage.js
@@ -7,11 +7,12 @@ const homePage = () => {
     const searchBar = document.createElement('div');
     searchBar.innerHTML = `
         <img src='/assets/icon-search.svg' alt='search'/>
-        <input type='text' id='searchVal'/>
+        <input type='text' id='searchVal' placeholder='Search for movies or TV series'/>
     `;
 
     main.appendChild(searchBar);
     createHeadings("Trending", main);
+    createHeadings("Recommended for you", main);
 
     return main
 }
