9baa9d8b213ba82a6df930c7ca3694fcd5fa84fd
MthoNtanzi <mthontanzi99@gmail.com>
2026-02-16 13:49:41 +0200

    make trending card dynamic

--- components/trendingCard.js
+++ components/trendingCard.js
@@ -1,42 +1,45 @@
-const trendingCard = () => {
+const trendingCard = (item) => {
     const cardDiv = document.createElement('div');
-
     cardDiv.classList = 'trendingCardContainer';
+    cardDiv.style.backgroundImage = `url('${item.thumbnail}')`;
 
-    const defBookmarkIcon = 'assets/icon-bookmark-empty.svg';
-    const bookmarkTrueIcon = 'assets/icon-bookmark-full.svg';
-    const movieIcon = 'assets/icon-category-movie.svg';
-    const tvSeriesIcon = 'assets/icon-category-tv.svg';
-    let isBookMark = false;
-    let isMovie = true;
-
+    // Wrapper for the boomkark icon... for styling
     const bookmarkWrapper = document.createElement('div');
     bookmarkWrapper.classList = 'bookmarkImgWrapper';
 
-    const isBookMarkImg = document.createElement('img');
-    isBookMark ? isBookMarkImg.src = bookmarkTrueIcon : isBookMarkImg.src = defBookmarkIcon;
-    isBookMarkImg.alt = "bookmark";
-    isBookMarkImg.classList = 'bookmarkImg';
+    const bookmarkImg = document.createElement('img');
+    bookmarkImg.src = item.isBookmarked 
+        ? 'assets/icon-bookmark-full.svg' 
+        : 'assets/icon-bookmark-empty.svg';
+    bookmarkImg.alt = "bookmark";
+    bookmarkImg.classList.add('bookmarkImg');
 
-    bookmarkWrapper.appendChild(isBookMarkImg);
+    bookmarkWrapper.appendChild(bookmarkImg);
 
+    // Description container, containing movie name and description
     const descriptionContainer = document.createElement('div');
     descriptionContainer.classList = "descriptionContainer";
 
     const movieDescription = document.createElement('p');
     movieDescription.classList = "movieDescription"
+
+    const categoryIcon = item.category === "Movie"
+        ? 'assets/icon-category-movie.svg'
+        : 'assets/icon-category-tv.svg';
+    
     movieDescription.innerHTML = `
-        <span id='movieYear'>2019</span> • <span id="category"> <img src='${isMovie? movieIcon: tvSeriesIcon}' alt='movie icon'/> Movie</span> • <span id='rating'>PG</span>
+        <span>${item.year}</span> • <span><img src='${categoryIcon}' alt='movie icon'/> ${item.category}</span> • <span>${item.rating}</span>
     `;
+
     const movieName = document.createElement('h1');
-    movieName.innerText = 'BeyondEarth';
+    movieName.innerText = item.title;
     movieName.classList = "movieName"
 
-    cardDiv.appendChild(bookmarkWrapper);
     descriptionContainer.appendChild(movieDescription);
     descriptionContainer.appendChild(movieName);
+
+    cardDiv.appendChild(bookmarkWrapper);
     cardDiv.appendChild(descriptionContainer);
-    cardDiv.style.backgroundImage = "url('assets/golfImg.jpg')";
 
     return cardDiv;
 }
--- pages/homePage.js
+++ pages/homePage.js
@@ -2,6 +2,15 @@ import createHeadings from '../components/functions.js';
 import searchBar from '../components/searchBar.js';
 import trendingCard from '../components/trendingCard.js';
 
+const fakeMovie = {
+    title: "Beyond Earth",
+    year: 2019,
+    category: "Movie",
+    rating: "PG",
+    isBookmarked: false,
+    thumbnail: "assets/golfImg.jpg"
+};
+
 
 const homePage = () => {
     const main = document.createElement('main');
@@ -9,7 +18,7 @@ const homePage = () => {
     main.appendChild(searchBar());
     createHeadings("Trending", main);
     // Trending Card
-    main.appendChild(trendingCard());
+    main.appendChild(trendingCard(fakeMovie));
 
     createHeadings("Recommended for you", main);
     // Movie Cards
