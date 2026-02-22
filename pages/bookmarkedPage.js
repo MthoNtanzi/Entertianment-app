ec29135b94b2c47a1aa76f1ad29645b9c13fe622
MthoNtanzi <mthontanzi99@gmail.com>
2026-02-22 18:32:04 +0200

    Create bookmark page and populate the page

--- pages/bookmarkedPage.js
+++ pages/bookmarkedPage.js
@@ -1,11 +1,52 @@
-
+import { getBookmarked } from "../api.js";
+import createHeadings from "../components/functions.js";
+import searchBar from "../components/searchBar.js";
+import movieGrid from "../components/movieGrid.js";
 
 const bookmarkedPage = () => {
     const main = document.createElement('main');
 
-    main.innerText = `Bookmarked Page`;
+    const allBookmarked = getBookmarked();
+    const bookmarkedMovies = allBookmarked.filter(item => item.category === 'Movie');
+    const bookmarkedSeries = allBookmarked.filter(item => item.category === 'TV Series');
+
+    const trendingMovieWrapper = document.createElement('div');
+    trendingMovieWrapper.classList.add("trendingMovieWrapper");
+    const trendingSeriesWrapper = document.createElement('div');
+    trendingSeriesWrapper.classList.add("trendingSeriesWrapper");
+
+    main.appendChild(searchBar());
+    main.appendChild(createHeadings("Bookmarked Movies"));
+    trendingMovieWrapper.appendChild(movieGrid(bookmarkedMovies));
+    main.appendChild(trendingMovieWrapper);
+    main.appendChild(createHeadings("Bookmarked TV Series"));
+    trendingSeriesWrapper.appendChild(movieGrid(bookmarkedSeries));
+    main.appendChild(trendingSeriesWrapper);
 
     return main
 }
 
 export default bookmarkedPage;
+
+// import { getTVSeries } from "../api.js";
+// import createHeadings from "../components/functions.js";
+// import searchBar from "../components/searchBar.js";
+// import movieGrid from "../components/movieGrid.js";
+
+// const tvSeriesPage = () => {
+//     const main = document.createElement('main');
+
+//     const allSeries = getTVSeries();
+
+//     main.appendChild(searchBar());
+//     main.appendChild(createHeadings("TV Series"));
+
+//     const seriesWrapper = document.createElement('div');
+//     seriesWrapper.classList.add("seriesWrapper");
+//     seriesWrapper.appendChild(movieGrid(allSeries));
+//     main.appendChild(seriesWrapper);
+
+//     return main
+// }
+
+// export default tvSeriesPage;
