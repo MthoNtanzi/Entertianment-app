8cb582f83e4ddfb82fb7a5a2b62144a91bca8438
MthoNtanzi <mthontanzi99@gmail.com>
2026-02-22 18:34:09 +0200

    Clean up

--- pages/bookmarkedPage.js
+++ pages/bookmarkedPage.js
@@ -27,26 +27,3 @@ const bookmarkedPage = () => {
 }
 
 export default bookmarkedPage;
-
-// import { getTVSeries } from "../api.js";
-// import createHeadings from "../components/functions.js";
-// import searchBar from "../components/searchBar.js";
-// import movieGrid from "../components/movieGrid.js";
-
-// const tvSeriesPage = () => {
-//     const main = document.createElement('main');
-
-//     const allSeries = getTVSeries();
-
-//     main.appendChild(searchBar());
-//     main.appendChild(createHeadings("TV Series"));
-
-//     const seriesWrapper = document.createElement('div');
-//     seriesWrapper.classList.add("seriesWrapper");
-//     seriesWrapper.appendChild(movieGrid(allSeries));
-//     main.appendChild(seriesWrapper);
-
-//     return main
-// }
-
-// export default tvSeriesPage;
