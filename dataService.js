29db485eca4cb215dc9bfd5db3cf30d2c651feab
MthoNtanzi <mthontanzi99@gmail.com>
2026-02-24 20:33:49 +0200

    Persist bookmark data to local storage

--- dataService.js
+++ dataService.js
@@ -1,26 +1,59 @@
 import data from './data.js';
 
+//save bookmarks to local storage
+const BOOKMARK_KEY = "entertainment-bookmarks";
+
+const getStoredBookmarks = () => {
+    return JSON.parse(localStorage.getItem(BOOKMARK_KEY)) || [];
+};
+
+const saveBookmarks = (bookmarks) => {
+    localStorage.setItem(BOOKMARK_KEY, JSON.stringify(bookmarks));
+}
+
 export const getTrendingContent = () => {
-    return data.filter(item => item.isTrending);
+    return applyBookmarks(
+        data.filter(item => item.isTrending)
+    );
 };
 
 export const getAllContent = () => {
-    return data;
+    return applyBookmarks(data);
 };
 
 export const getMovies = () => {
-    return data.filter(item => item.category === 'Movie');
+    return applyBookmarks(data.filter(item => item.category === 'Movie'));
 };
 
 export const getTVSeries = () => {
-    return data.filter(item => item.category === 'TV Series');
+    return applyBookmarks(data.filter(item => item.category === 'TV Series'));
 };
 
 export const getBookmarked = () => {
-    return data.filter(item => item.isBookmarked);
+    return applyBookmarks(data.filter(item => item.isBookmarked));
 };
 
 export const toggleBookmark = (title) => {
-    const item = data.find(item => item.title === title);
-    if (item) item.isBookmarked = !item.isBookmarked;
+    const bookmarks = getStoredBookmarks();
+
+    const exists = bookmarks.includes(title);
+
+    let updated;
+
+    if (exists) {
+        updated = bookmarks.filter(t => t !== title);
+    } else {
+        updated = [...bookmarks, title];
+    }
+
+    saveBookmarks(updated);
+};
+
+const applyBookmarks = (items) => {
+    const bookmarks = getStoredBookmarks();
+
+    return items.map(item => ({
+        ...item,
+        isBookmarked: bookmarks.includes(item.title)
+    }));
 };
